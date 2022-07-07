b021353fa8f544e0cb864a393ba2520a7b60a501
Brian Mawira <58627423+brianblue05@users.noreply.github.com>
2022-07-08 00:03:00 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,9 +11,7 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0 || i % 5 == 0)
-		{
-			if (i % 3 == 0)
+		if (i % 3 == 0)
 			{
 				printf("Fizz");
 			}
@@ -26,6 +24,7 @@ int main(void)
 			printf("%d", i);
 		}
 		if (i != 100)
+		{
 			printf(" ");
 	}
 	printf("\n");
