04a587784cad6e42ea6568bdcc237400f22f9528
Brian Mawira <58627423+brianblue05@users.noreply.github.com>
2022-07-08 01:40:35 +0300

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,9 +9,6 @@ int main(void)
   int i;
   for (i=1; i<=100; i++)
   {
-    // number divisible by 3 and 5 will
-    // always be divisible by 15, print
-    // 'FizzBuzz' in place of the number
     if (i%15 == 0)  
       printf ("FizzBuzz\t");
     else if ((i%3) == 0)
