98f4dcdbcd2268a6d6cb89257bf46946d06430ec
Brian Mawira <58627423+brianblue05@users.noreply.github.com>
2022-07-14 22:59:13 +0300

    Update main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,9 @@
 #ifndef _main_h_
 #define _main_h_
-
+/**
+* _strcat - Contains the string
+* @src - source of the string
+* @dest - destination of string
+**/
 char *_strcat(char *dest, char *src);
 #endif
