3cd15a02e0d77516c2b156d476b9a7c52a3f15c7
Brian Mawira <58627423+brianblue05@users.noreply.github.com>
2022-07-20 15:16:45 +0300

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef "_main_h_"
-#define "_main_h_"
+#ifndef main_h
+#define main_h
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
