c2a399696a18260d1284ba4db7e05ebb2935ac0c
Brian Mawira <58627423+brianblue05@users.noreply.github.com>
2022-07-26 22:21:39 +0300

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *  * create_array - creates array
- *   * @size: size of array to create
- *    * @c: char to initialize with?
- *     * Return: char value
- *      */
+ * create_array - creates array
+ * @size: size of array to create
+ * @c: char to initialize with?
+ * Return: char value
+ **/
 char *create_array(unsigned int size, char c)
 {
 		char *t;
