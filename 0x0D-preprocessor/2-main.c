8f1ff0a54adf9d3943b042fff3f55269eb23a62a
Brian Mawira <brianmawira05@gmail.com>
2022-08-01 22:49:42 +0300

    Add 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- *  * main - main file returns file name
- *   * Return: returns 0 always
- *    */
+*main - main file returns file name
+*Return: returns 0 always
+ **/
 int main(void)
 {
-		printf("%s\n", __FILE__);
-			return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
