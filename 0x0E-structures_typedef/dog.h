dc44223f6436c680223f687bd8cde5436fa14350
Brian Mawira <58627423+brianblue05@users.noreply.github.com>
2022-08-01 23:46:35 +0300

    Fix Betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,16 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 /**
- *  * struct dog - structure of dog
- *   * @name: name of dog
- *    * @age: age of dog
- *     * @owner: owner of dog
- *      */
+ * struct dog - structure of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
 struct dog
 {
-		char *name;
-			float age;
-				char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
