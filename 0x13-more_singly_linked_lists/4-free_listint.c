a507410f7dbc8acad87f5d7533dffd453efe1a6d
Brian Mawira <58627423+brianblue05@users.noreply.github.com>
2022-08-16 00:46:12 +0300

    Update 4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -2,18 +2,14 @@
 /**
  * free_listint - frees the list
  * @head: head of struct
- *
  */
 void free_listint(listint_t *head)
 {
 	listint_t *tmp;
-
-
 	while (head != NULL)
 	{
 		tmp = head;
 		free(head);
 		head = tmp->next;
 	}
-
 }
