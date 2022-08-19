6b4746ff2361e41c171530fe538c973c1e4e475b
Brian Mawira <58627423+brianblue05@users.noreply.github.com>
2022-08-19 23:33:39 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,24 +1,19 @@
+#include <stdlib.h>
 #include "main.h"
-/**
- * binary_to_uint - binary to uint
- * @b: string to convert
- * Return: unsigned int
- **/
-unsigned int binary_to_uint(const char *b)
-{
-	unsigned int x = 0;
 
-	if (b == NULL)
-		return (0);
-	while (*b)
-	{
-		if (*b == '1')
-			x = (x << 1) | 1;
-		else if (*b == '0')
-			x <<= 1;
-		else
-			return (0);
-		b++;
-	}
-	return (x);
+/**
+ * set_bit - sets the value of a bit to 1 at a given index.
+ * @n: Pointer to a decimal number
+ * @index: Index
+ *
+ * Return: Nothing (void).
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == NULL || index > 63)
+		return (-1);
+
+	*n = *n | 1 << index;
+	return (1);
 }
